1e5799833ed91ae0e7162c19c8ebf223b6a58e07
Mustafa Aktaş <gokberkmustafa@outlook.com>
2022-03-27 11:58:47 +0300

    DoublyLinkedListTests

---
+++ LinkedListTest/DoublyLinkedListTest.cs
@@ -0,0 +1,26 @@
+﻿using LinkedList.DoublyLinkedList;
+using Xunit;
+
+namespace LinkedListTest
+{
+    public class DoublyLinkedListTest
+    {
+        private DoublyLinkedList<char> _list;
+        public DoublyLinkedListTest()
+        {
+            _list = new DoublyLinkedList<char>(new char[] {'a','z'});
+        }
+        [Theory]
+        [InlineData('d')]
+        [InlineData('r')]
+        [InlineData('y')]
+        public void AddFirst_Test(char value)
+        {
+            //act
+            _list.AddFirst(value);
+
+            //assert
+            Assert.Equal(value, _list.Head.Value);
+        }
+    }
+}
